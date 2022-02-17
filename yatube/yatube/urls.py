1597a87b313d415a4b2522cd34ea514f81cdc683
SaPer663 <saper663ru@gmail.com>
2022-02-17 16:45:57 +0500

    added api

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('django.contrib.auth.urls')),
     path('about/', include('about.urls', namespace='about')),
+    path('api/', include('api.urls')),
     path('', include('posts.urls', namespace='posts')),
 ]
 
